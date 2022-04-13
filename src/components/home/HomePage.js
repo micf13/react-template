52eb8dc94f06eadf7fd650e671445de7b3e27009
micf13 <87240560+micf13@users.noreply.github.com>
2022-04-13 16:22:26 +0100

    Added basic hook examples
    Added basic hook examples

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 
-const HomePage = () => (
-    <div className="jumbotron"><h1>Home Page</h1></div>
-)
+function HomePage()
+{
+    const [Emails, setEmails] = useState(0);
+    const [User, setUser]= useState("");
+    const [LoggedIn, setLoggedIn]= useState(false);
+    const userNameInput = useRef();
+    useEffect(() => {
+        document.title = `Messages: ${Emails}`;
+    });
+    const setFocusToInputBox = () => {
+        userNameInput.current.focus();
+        setUser(userNameInput.current.value)
+        if(User !== "")
+        toggleLoginStatus();
 
+    }
+    const toggleLoginStatus = () => {
+        setLoggedIn(!LoggedIn)
+    }
+    const toggleMessage = () => {
+        if(User === "")
+        {
+            return(
+                <p>{User} Messages: {Emails}</p>
+            )
+        }
+        else{
+            return(
+                <p>{User}'s Messages: {Emails}</p>
+            )
+        }
+
+    }
+return(
+        <div className="jumbotron">
+            <h1>Home Page</h1>
+            <input type="text"  ref={userNameInput}></input><button onClick={setFocusToInputBox}>Set Username</button>
+            {toggleMessage()}
+
+            <button className="button-grn" onClick={() => setEmails(Emails+1)}>+</button>
+            <span>  </span>
+            <button className="button-grn" onClick={() => setEmails(Emails-1)}>-</button>
+        </div>
+    )
+}
 export default HomePage;
